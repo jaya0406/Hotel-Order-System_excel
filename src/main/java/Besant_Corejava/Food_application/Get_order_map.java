4c5672537814f6b5f32c1c68ecb120d4d7335d56
jaya0406 <166355221+jaya0406@users.noreply.github.com>
2024-05-17 21:52:53 +0530

    Update Get_order_map.java

--- src/main/java/Besant_Corejava/Food_application/Get_order_map.java
+++ src/main/java/Besant_Corejava/Food_application/Get_order_map.java
@@ -1,117 +1,63 @@
 package Besant_Corejava.Food_application;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import org.apache.poi.ss.usermodel.CellType;
-
 import org.apache.poi.ss.usermodel.Row;
-
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
-
-public class Get_order_map extends Hotel_map {
-	
-public void get_order() throws IOException, InterruptedException
-{
+public class Get_order_map extends Hotel_map
+{	
+	public void get_order() throws IOException, InterruptedException
+	{
 		get_hotel();
 		if(isthere==true)
 		{
 			File f = new File(filepath);
 			FileInputStream fis = new FileInputStream(f);
-
-			try (XSSFWorkbook wbk = new XSSFWorkbook(fis)) {
-				for(int sheetcount = 0 ;sheetcount<wbk.getNumberOfSheets();sheetcount++)
-				{
-					sheet = wbk.getSheetAt(sheetcount);
-						 
+			XSSFWorkbook wbk = new XSSFWorkbook(fis);
+			
+			for(int sheetcount = 0 ;sheetcount<wbk.getNumberOfSheets();sheetcount++)
+			{
+				sheet = wbk.getSheetAt(sheetcount);		 
 				if (user_hotel.equalsIgnoreCase(sheet.getSheetName())) 
-				 {
-							
+				 {		
 					 System.out.print("\nEnter Item: ");
-					 user_item = s.nextLine().trim().toUpperCase();
-				                          
-				for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) 
-				{ // first row is the header
-					row = sheet.getRow(rowIndex);
-					if (row != null) 
-					 {
-						foodcell = row.getCell(0);
-					        pricecell = row.getCell(1);
+					 user_item = s.nextLine().trim().toUpperCase();                  
+			for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) 
+			{ // first row is the header
+				row = sheet.getRow(rowIndex);
+				if (row != null) 
+					{
+					        foodcell = row.getCell(0);
+					    	pricecell = row.getCell(1);
 					                    
-					 if (foodcell != null && foodcell.getCellType() == CellType.STRING) 
-					  {
-					 if(pricecell!=null && pricecell.getCellType()==CellType.NUMERIC)
-					  {
+				 if (foodcell != null && foodcell.getCellType() == CellType.STRING) 
+					{
+				 if(pricecell!=null && pricecell.getCellType()==CellType.NUMERIC)
+					{
 					        String foodItem = foodcell.getStringCellValue().toUpperCase();
 					        int price = (int) pricecell.getNumericCellValue();
 									
-					        Food_menu.put(foodItem,price);
-					        if(Food_menu.containsKey(user_item))
-					        {
-					             isitem=true;
-					             System.out.print("Enter quantity: ");
-					             user_quantity = s.nextInt();
-					             System.out.println("Your order "+ user_item+" - "+user_quantity);
-					             amount =Food_menu.get(user_item)*user_quantity;
-					            break;
-					        }
+				 Food_menu.put(foodItem,price);
+				if(Food_menu.containsKey(user_item))
+					{
+					        isitem=true;
+					        System.out.print("Enter quantity: ");
+					        user_quantity = s.nextInt();
+					        System.out.println("Your order "+ user_item+" - "+user_quantity);
+					        amount =Food_menu.get(user_item)*user_quantity;
+					        break;
+					}
 					} 	
 					}
 					}
+			}
 				}
-				}
-				}
+			}
 			} 
-
-			XSSFWorkbook wbk = new XSSFWorkbook(fis);
-			
-		for(int sheetcount = 0 ;sheetcount<wbk.getNumberOfSheets();sheetcount++)
-		{
-			sheet = wbk.getSheetAt(sheetcount);
-				 
-		if (user_hotel.equalsIgnoreCase(sheet.getSheetName())) 
-		 {
-					
-			 System.out.print("\nEnter Item: ");
-			 user_item = s.nextLine().trim().toUpperCase();
-	                              
-		for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) 
-		{ // first row is the header
-			row = sheet.getRow(rowIndex);
-			if (row != null) 
-			 {
-				foodcell = row.getCell(0);
-			        pricecell = row.getCell(1);
-			                    
-			 if (foodcell != null && foodcell.getCellType() == CellType.STRING) 
-			  {
-			 if(pricecell!=null && pricecell.getCellType()==CellType.NUMERIC)
-			  {
-			        String foodItem = foodcell.getStringCellValue().toUpperCase();
-			        int price = (int) pricecell.getNumericCellValue();
-							
-			        Food_menu.put(foodItem,price);
-			        if(Food_menu.containsKey(user_item))
-			        {
-			             isitem=true;
-			             System.out.print("Enter quantity: ");
-			             user_quantity = s.nextInt();
-			             System.out.println("Your order "+ user_item+" - "+user_quantity);
-			             amount =Food_menu.get(user_item)*user_quantity;
-			            break;
-			        }
-			} 	
-			}
-			}
-		}
-		}
-		} 
-
-		}
+		
 		else
 		{
 			return;
@@ -123,8 +69,6 @@ public void get_order() throws IOException, InterruptedException
 			return;
 		}
 }	
-
-
 	public static void main(String[]args) throws IOException, InterruptedException
 	{
 		Get_order_map gom= new Get_order_map();
