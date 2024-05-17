55c29f5aadf696d1071654e863bfde32f8f16f35
jaya0406 <166355221+jaya0406@users.noreply.github.com>
2024-05-17 18:14:48 +0530

    Update Get_order.java

--- src/main/java/Besant_Corejava/Food_application/Get_order.java
+++ src/main/java/Besant_Corejava/Food_application/Get_order.java
@@ -1,14 +1,10 @@
 package Besant_Corejava.Food_application;
 
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class Get_order extends Hotel{
 	protected boolean isitem=false;
@@ -17,47 +13,23 @@ public class Get_order extends Hotel{
 		search_hotel();
 		if(super.isthere==true)
 		{
-			
+		
 			System.out.print("\nEnter Item: ");
 			user_item = s.nextLine().trim();
-			
-			File f = new File(filepath);
-			FileInputStream fis = new FileInputStream(f);
-			XSSFWorkbook wbk = new XSSFWorkbook(fis);
-			
-			for(int sheetcount = 0 ;sheetcount<wbk.getNumberOfSheets();sheetcount++)
+							
+			Food=Foodcell.getStringCellValue();
+			if(user_item.equalsIgnoreCase(Food))
 			{
-				Sheet sheet = wbk.getSheetAt(sheetcount);
-				if(user_hotel.equalsIgnoreCase(sheet.getSheetName()))
+				isitem=true;
+				System.out.println("Enter Qantity: ");
+				user_quantity = s.nextInt();
+				if(Pricecell!=null && Pricecell.getCellType()==CellType.NUMERIC)
 				{
-					for(Row row : sheet)
-					{
-						Cell Foodcell = row.getCell(0);
-						Cell Pricecell = row.getCell(1);
-						
-						if(Foodcell!= null && Foodcell.getCellType()==CellType.STRING)
-						{
-							Food=Foodcell.getStringCellValue();
-							if(user_item.equalsIgnoreCase(Food))
-							{
-								isitem=true;
-								System.out.println("Enter Qantity: ");
-								user_quantity = s.nextInt();
-								if(Pricecell!=null && Pricecell.getCellType()==CellType.NUMERIC)
-								{
-									
-									Price=Pricecell.getNumericCellValue();
-									System.out.println("Your Order - "+user_item.toUpperCase()+" "+Price+" - "+user_quantity);
-									break;
-								}
-							}
-						}
-					}
-					break;
+					Price=Pricecell.getNumericCellValue();
+					System.out.println("Your Order - "+user_item.toUpperCase()+" "+Price+" - "+user_quantity);
+
 				}
-				
 			}
-			wbk.close();
 		}
 		else
 		{
@@ -68,8 +40,6 @@ public class Get_order extends Hotel{
 			System.out.println("Error: Item not found.");
 			return;
 		}
-		
-
 	}
 
 	public static void main(String[] args) throws IOException
